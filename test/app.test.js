14764a450415edbfc627de6e1cef36df7320f15f
oryaaharoni <oryaaharonii@gmail.com>
2024-05-29 15:59:02 +0300

    dockerfile, ci.yml

--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,12 @@
-const app = require('../app.js');
+const app = require('../src/app.js');
 const supertest = require('supertest');
 const request = supertest(app);
 
 
 describe('GET /', () => {
     it('responds with AM ISRAEL HAI!', async () => {
-        const res = await request.get('/');
 
+        const res = await request.get('/');
         expect(res.statusCode).toEqual(200);
         expect(res.text).toBe('AM ISRAEL HAI!');
     });
