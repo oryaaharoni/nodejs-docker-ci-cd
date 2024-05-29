238cd08bfb4eee584325e2a5f478b3b4fd81a0bb
oryaaharoni <oryaaharonii@gmail.com>
2024-05-29 14:22:10 +0300

    app.test.js file

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = require('./app.js')
+const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send('AM ISRAEL HAI!');    
-});
-
-app.listen(PORT, () =>{
-    console.log(`App is running on port ${PORT}`);
+app.listen(port, () =>{
+    console.log(`App is running on port ${port}`);
 });
---
+++ test/app.test.js
@@ -0,0 +1,13 @@
+const app = require('../app.js');
+const supertest = require('supertest');
+const request = supertest(app);
+
+
+describe('GET /', () => {
+    it('responds with AM ISRAEL HAI!', async () => {
+        const res = await request.get('/');
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.text).toBe('AM ISRAEL HAI!');
+    });
+});
